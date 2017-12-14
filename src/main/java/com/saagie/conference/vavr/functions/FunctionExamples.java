0f3f7e0994ad0022dfcf9ed382aac2f192a8bec5
Guillaume Lours <guillaume@saagie.com>
2017-12-14 15:25:04 +0100

    refactoring: change composition functions examples

--- src/main/java/com/saagie/conference/vavr/functions/FunctionExamples.java
+++ src/main/java/com/saagie/conference/vavr/functions/FunctionExamples.java
@@ -21,22 +21,22 @@ public class FunctionExamples {
     }
 
 
-    private Function1<Option<User>, Option<String>> lastName = user -> user.map(User::getLastName);
-    private Function1<Option<String>, Option<String>> toUpperCase = value -> value.map(String::toUpperCase);
-    private Function1<Option<User>, Option<String>> lastNameInUpperCase = lastName.andThen(toUpperCase);
+    private Function1<User, String> lastName = User::getLastName;
+    private Function1<String, String> toUpperCase = String::toUpperCase;
+    private Function1<User, String> lastNameInUpperCase = lastName.andThen(toUpperCase);
 
     private Function1<Option<User>, Address> getAddressWithSideEffect = user -> user.get().getAddress();
     private Function1<Option<User>, Option<Address>> safeGetAddress = Function1.lift(getAddressWithSideEffect);
 
     private Function4<Integer, Integer, Integer, Integer, Integer> sum = (a, b, c, d ) -> a + b + c + d;
     private Function1<Integer, Integer> partialApplicationFunc = sum.apply(1,2,3);
-    private Function1<Integer, Function1<Integer, Function1<Integer, Integer>>> curriedFunc = sum.curried().apply(5);
+    private Function1<Integer, Function1<Integer, Function1<Integer, Integer>>> add3ValuesWith5 = sum.curried().apply(5);
 
     private Function0<Double> memoizedRandom = Function0.of(Math::random).memoized();
 
     public String userLastNameToUpperCase(String userName) {
-        return lastNameInUpperCase
-                .apply(usersVavr.get(userName))
+        return usersVavr.get(userName)
+                .map(lastNameInUpperCase)
                 .getOrElse("User Not Found");
     }
 
@@ -53,7 +53,7 @@ public class FunctionExamples {
     }
 
     public int currying(int val1, int val2, int val3) {
-        return curriedFunc.apply(val1).apply(val2).apply(val3);
+        return add3ValuesWith5.apply(val1).apply(val2).apply(val3);
     }
 
     public double memoize() { return memoizedRandom.apply(); }
