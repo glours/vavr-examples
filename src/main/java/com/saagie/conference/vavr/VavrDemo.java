5e278bffb5d54be9aeb16341dd9bc10ff5da4f75
Guillaume Lours <guillaume@saagie.com>
2017-12-14 15:40:42 +0100

    refactoring: adapt command line pattern matching example

--- src/main/java/com/saagie/conference/vavr/VavrDemo.java
+++ src/main/java/com/saagie/conference/vavr/VavrDemo.java
@@ -20,14 +20,15 @@ public class VavrDemo
         Option<String> arg = Array.of(args).headOption();
         String commandDescription = API.Match(arg.getOrElse("")).of(
                 Case($(isIn("-h", "--help")), helpDocumentation.apply()),
-                Case($(isIn("-v", "--version")), versionDocumentation.apply(Option.none()) ),
-                Case($(), invalidCommand.apply(helpDocumentation.apply(), arg))
-        ).getOrElse("Error when parsing argument");
+                Case($(isIn("-v", "--version")), versionDocumentation.apply("") ),
+                Case($(), invalidCommand.apply(helpDocumentation.apply(), arg.getOrElse("")))
+        );
         System.out.println(commandDescription);
     }
 
-    private static Function0<Option<String>> usageDocumentation =  () -> Option.of("usage: VavrDemo [options] \n-h, --help : Display command line help\n") ;
-    private static Function1<Option<String>, Option<String>> versionDocumentation = previous -> previous.map(prev -> prev + "-v, --version : Display version Information");
-    private static Function0<Option<String>> helpDocumentation = usageDocumentation.andThen(versionDocumentation);
-    private static Function2<Option<String>, Option<String>, Option<String>> invalidCommand = (previous, arg) -> previous.map(prev -> "Unable to parse command line option : " + arg.getOrElse("") +"\n" + prev) ;
+    private static Function0<String> usageDocumentation =  () -> "usage: VavrDemo [options] \n-h, --help : Display command line help\n" ;
+    private static Function1<String, String> versionDocumentation = previous -> previous.concat("-v, --version : Display version Information");
+    private static Function0<String> helpDocumentation = usageDocumentation.andThen(versionDocumentation);
+    private static Function2<String, String, String> invalidCommand = (previous, arg) -> "Unable to parse command line option : " + arg +"\n" + previous;
+
 }
